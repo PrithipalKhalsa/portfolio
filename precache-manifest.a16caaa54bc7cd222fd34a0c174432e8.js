61b71c3711d84ebab42c20f5d4ecc97e0626fe77
prithipal khalsa <prithipalkhalsa@prithipals-MBP.home>
2020-06-03 18:51:22 -0700

    Updates

---
+++ precache-manifest.a16caaa54bc7cd222fd34a0c174432e8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "955712b903d00442b321f736b5757291",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "12e3e863e9d4170e847e",
+    "url": "/portfolio/static/css/main.531612e4.chunk.css"
+  },
+  {
+    "revision": "bd7fa96efd0657268f79",
+    "url": "/portfolio/static/js/2.0e1c66b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.0e1c66b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12e3e863e9d4170e847e",
+    "url": "/portfolio/static/js/main.4c87e0cb.chunk.js"
+  },
+  {
+    "revision": "88fbd3d397ae3690d5f2",
+    "url": "/portfolio/static/js/runtime-main.fb1b3dac.js"
+  },
+  {
+    "revision": "7687d477600f057f3d07b4ff4fb5d328",
+    "url": "/portfolio/static/media/cv.7687d477.pdf"
+  },
+  {
+    "revision": "3480db5eabd3ef35cf349caa44c5171e",
+    "url": "/portfolio/static/media/git.3480db5e.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "87fb710d38630ce000920046849520ff",
+    "url": "/portfolio/static/media/me.87fb710d.jpeg"
+  }
+]);
