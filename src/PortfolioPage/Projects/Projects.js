90dccedc72bec5d48e65885b40841ff6be3a5d06
prithipal khalsa <prithipalpsk@gmail.com>
2020-06-08 09:12:29 -0700

    added content

--- src/PortfolioPage/Projects/Projects.js
+++ src/PortfolioPage/Projects/Projects.js
@@ -8,9 +8,17 @@ const Projects = () => {
   const testData=[{
     title: "Matchy Words",
     description:"Inspired by the hours of zoom sessions during the Covid-19 isolation: a real time browser multiplayer game. Essentially a free form apples to apples game. ",
-    stack:["Node.js","React.js","express.js","Socket.io","Gcloud","Nodemailer"],
+    stack:["Node.js","React.js","express.js","Socket.io","Gcloud"],
     link:"https://matchywords.com/",
     github:"https://github.com/PrithipalKhalsa/matchy-words"
+  },
+  {
+    title: "MP Watch",
+    description:"This app scrapes the Mountain Project For Sale page once every hour and will send you an email if a key word is mentioned. ",
+    stack:["Node.js","React.js","express.js","MongoDB","Gcloud",],
+    link:"https://mp-watch.appspot.com/",
+    github:"https://github.com/PrithipalKhalsa/mp-watch"
+
   },
     {
     title: "Spottvenn",
@@ -23,8 +31,11 @@ const Projects = () => {
   return (
 
 <div className="Projects">
-<h1>Personal Projects</h1>
 <div className="project-group">
+<div className="project-title">
+<h1>Personal Projects</h1>
+<p className="project-subtitle">Here are a few of my favorite personal projects, any private repository can be revealed upon request. If you would like to collaberate on a project please do not hesitate to reach out.</p>
+</div>
                {testData.map(({stack,title,description,link,github}) =>
                   <div className="card-holders" key={title}><Card stack={stack} title={title}description={description} link={link} github={github}/></div>)}
              </div>
